f5a3e9b8eb5dc9cb12304163d184447a8ed8c10d
Tacomixen <hampus.hasslof@gmail.com>
2021-04-20 11:25:14 +0200

    New timer function

--- app.py
+++ app.py
@@ -8,6 +8,8 @@ import firebase_admin
 from google.cloud import secretmanager
 from google.oauth2 import service_account
 
+from threading import Timer,Thread,Event
+
 GOOGLE_CLOUD_PROJECT_NUMBER = 544247596163
 FIREBASE_SA_SECRET_NAME = 'firebase-key'
 VERSION = 'latest'
@@ -32,53 +34,28 @@ sensors_ref = db.collection('sensor_data')
 app = Flask(__name__)
 CORS(app, resources={r"/*": {"origins": "*"}})
 
-from threading import Timer
+class MyThread(Thread):
+    def __init__(self, event):
+        Thread.__init__(self)
+        self.stopped = event
 
-
-class InfiniteTimer():
-    """A Timer class that does not stop, unless you want it to."""
-
-    def __init__(self, seconds, target):
-        self._should_continue = False
-        self.is_running = False
-        self.seconds = seconds
-        self.target = target
-        self.thread = None
-
-    def _handle_target(self):
-        self.is_running = True
-        self.target()
-        self.is_running = False
-        self._start_timer()
-
-    def _start_timer(self):
-        if self._should_continue: # Code could have been running when cancel was called.
-            self.thread = Timer(self.seconds, self._handle_target)
-            self.thread.start()
-
-    def start(self):
-        if not self._should_continue and not self.is_running:
-            self._should_continue = True
-            self._start_timer()
-        else:
-            print("Timer already started or running, please wait if you're restarting.")
-
-    def cancel(self):
-        if self.thread is not None:
-            self._should_continue = False # Just in case thread is running and cancel fails.
-            self.thread.cancel()
-        else:
-            print("Timer never started or failed to initialize.")
+    def run(self):
+        while not self.stopped.wait(5):
+            print("my thread")
+            # call a function
+            tick()
 
 
 def tick():
     print('T E S T L O G G')
 
-# Example Usage
-t = InfiniteTimer(5, tick)
-t.start()
-
 
 
 if __name__ == "__main__":
     app.run(debug=True,host='0.0.0.0',port=int(os.environ.get('PORT', 8080)))
+
+    stopFlag = Event()
+    thread = MyThread(stopFlag)
+    thread.start()
+    # this will stop the timer
+    #stopFlag.set()
